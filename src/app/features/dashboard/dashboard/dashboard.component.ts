d4d3cda2cf6e3581aee018cff4445a16e6088e93
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2026-07-07 21:48:49 -0300

    fix: Fix dashboard style

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import { MatIcon } from "@angular/material/icon";
+import { UserService } from '../../users/services/auth.service';
 
 interface Agendamento {
   cliente: string;
@@ -12,6 +13,14 @@ interface Agendamento {
   modalidade: 'TENNIS' | 'PADEL';
 }
 
+interface Court {
+  id: string;
+  name: string;
+  sport: 'FUTEBOL' | 'BASQUETE' | 'VÔLEI';
+  status: 'DISPONÍVEL' | 'EM USO' | 'MANUTENÇÃO';
+  price: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -20,7 +29,32 @@ interface Agendamento {
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  protected userService = inject(UserService);
   today = signal<Date>(new Date());
+
+  courts: Court[] = [
+    {
+      id: '01',
+      name: 'Flow FC',
+      sport: 'FUTEBOL',
+      status: 'DISPONÍVEL',
+      price: 130.00
+    },
+    {
+      id: '02',
+      name: 'Quebrada FC',
+      sport: 'BASQUETE',
+      status: 'EM USO',
+      price: 159.00
+    },
+    {
+      id: '03',
+      name: 'Loucura FC',
+      sport: 'VÔLEI',
+      status: 'MANUTENÇÃO',
+      price: 159.00
+    }
+  ]
 
   
   // Massa de dados correspondente à tabela da imagem
--- src/app/features/users/services/auth.service.ts
+++ src/app/features/users/services/auth.service.ts
@@ -1,13 +1,37 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { UserLoginFormModel, UserRegisterFormModel } from '../../../shared/interfaces/User';
 import { HttpClient } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
 import { tap } from 'rxjs';
+
+export interface UserAuthenticated {
+  id: string;
+  name?: string;
+  email?: string;
+  role?: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private http = inject(HttpClient);
-
   private tokenSignal = signal<string | null>(null);
+
+  readonly currentUser = computed<UserAuthenticated | null>(() => {
+    const token = this.tokenSignal();
+    if (!token) return null;
+
+    try {
+      const decoded: any = jwtDecode(token);
+      return {
+        id: decoded.sub,
+        name: decoded.name,
+        role: decoded.role
+      };
+    } catch (error) {
+      console.error('Token JWT inválido:', error);
+      return null;
+    }
+  });
 
   get accessToken(): string | null {
     return this.tokenSignal();
